168a84c73967d381294492db2b584052aae8b916
Sanae Elamri <assanaae@gmail.com>
2024-06-21 20:42:57 +0100

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ *
+ * main - prints the alphabet except e and q 
+ * followed by a new line
+ * Return: returns 0, completed successfuly
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			printf(c);
+	}
+	putchar('\n');
+	return (0);
+}
