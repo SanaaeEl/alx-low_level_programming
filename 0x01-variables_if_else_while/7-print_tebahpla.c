f42f4b2bdd165d02cfbf7493166b9751b4d586b0
Sanae Elamri <assanaae@gmail.com>
2024-06-21 21:47:15 +0100

    print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ *
+ * main - prints the alphabet in reverse followed by a new line
+ * Return: returns 0, completed successfuly
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
