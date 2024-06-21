f06c08993648c80a0389405182858c4eff1ada38
Sanae Elamri <assanaae@gmail.com>
2024-06-21 20:32:12 +0100

    lower_c then upper_c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+ *
+ * main - prints the alphabet in lower case
+ * then in upper case followed by a new line
+ * Return: returns 0, completed successfuly
+ *
+ */
+
+int main(void)
+{
+	char c, u_c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+        {
+                putchar(u_c);
+        }
+	putchar('\n');
+	return (0);
+}
