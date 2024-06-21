c4cdb826cce4f946deeb7df23482cf0114d7161c
Sanae Elamri <assanaae@gmail.com>
2024-06-21 22:04:59 +0100

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,8 +16,11 @@ int main(void)
 	for (c = 0; c <= 9; c++)
 	{
 		putchar('0' + c);
-		putchar(',');
-		putchar(' ');
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
