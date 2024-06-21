2209b949b16c0fd1e0d96b4e6a19c79e9801a036
Sanae Elamri <assanaae@gmail.com>
2024-06-21 18:47:41 +0100

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,13 +2,11 @@
 
 /**
  * main - Entry point
- * printf -  prints the size of various types
- * return int always returns 0, indicating successful completion
+ * Description:  prints the sizeof various types.
+ * Return: 0 always returns 0, indicating successful completion.
  */
-
 int main(void)
 {
-	
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
