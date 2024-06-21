46aa567881278be6146a4e5ae08d8df76679ff73
Sanae Elamri <assanaae@gmail.com>
2024-06-21 01:25:19 +0100

    puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- * main - description : prints "Programming is like building a multilingual puzzle
+ * description : prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
 
