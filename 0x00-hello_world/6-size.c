c8f680510b80a5d742fd985dfa46f5051cae75c4
Sanae Elamri <assanaae@gmail.com>
2024-06-21 02:31:47 +0100

    sizeof() function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Return : 0, no error
+ *
+ */
+
+int main(void)
+{
+	/* function sizeof calculates the varialbe size*/
+	printf("Size of a char: %a byte(s) \n",sizeof(char));
+	printf("Size of a char: %a byte(s) \n",sizeof(int));
+	printf("Size of a char: %a byte(s) \n",sizeof(long int));
+	printf("Size of a char: %a byte(s) \n",sizeof(long long int));
+	printf("Size of a char: %a byte(s) \n",sizeof(float));
+	return (0);
