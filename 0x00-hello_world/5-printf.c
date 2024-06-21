e0e3e99daa1ea0bc83d717cecc98773b68f5c17c
Sanae Elamri <assanaae@gmail.com>
2024-06-21 01:37:25 +0100

    and now using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *
+ * main : prints a sentence using printf function
+ * Return : 0, no error
+ *
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
