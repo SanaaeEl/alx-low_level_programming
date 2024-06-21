b0e1a7baa18417b2799f6e9d19bc3f8872c39703
Sanae Elamri <assanaae@gmail.com>
2024-06-21 02:00:16 +0100

    and now using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /**
  * main - prints a sentence using printf function
- * return : 0, succesful execution
  *
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return (0); /*return statement indicates successful completion*/
 }
