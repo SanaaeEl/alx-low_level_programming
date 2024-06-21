5ae503759d9bf57b9902a2fde1381a24705afd2e
Sanae Elamri <assanaae@gmail.com>
2024-06-21 20:17:23 +0100

    alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.f>
+
+/*
+ *
+ * main - prints the alphabet followed by a new line
+ * Return: returns 0, completed successfuly
+ *
+ */
+
+int main(voi)
+{
+	char c;
+
+	for (c = 'z'; c <= 'z'; c++)
+		putchar(c);
+	putchar("\n");
+	return (0);
+}
