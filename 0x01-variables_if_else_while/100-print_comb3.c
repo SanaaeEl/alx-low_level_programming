fd31f99a3df7a675a4585b2462b9d7f72a28acad
Sanae Elamri <assanaae@gmail.com>
2024-06-22 01:36:23 +0100

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,17 +8,18 @@
 int main(void)
 {
 	int a, b;
+
 	for (a = 0; a <= 8; a++)
 	{
 		for (b = 1; b <= 9; b++)
 		{
 			putchar('0' + a);
 			putchar('0' + b);
+			if (a == 8 && b == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
-		if (a == 8 && b == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
