9739bc65aa174592c34ff1913095f829e83c4a35
Sanae Elamri <assanaae@gmail.com>
2024-06-22 02:50:42 +0100

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,9 +23,10 @@ int main(void)
 				putchar(b % 10 + '0');
 			}
 			if (a != 98 || b == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
