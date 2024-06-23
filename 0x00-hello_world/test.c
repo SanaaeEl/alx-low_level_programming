015ec8359c5c37c37b1f76f6f1bdadcaf616de20
Sanae Elamri <assanaae@gmail.com>
2024-06-23 12:55:09 +0100

    intel

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: test.
+ * for: testing answers.
+ * Return: 0 if completed successfuly.
+*/
+int main(void)
+{
+	a = 0;
+	for (; a < b;)
+	{
+		printf("%d\n", a++);
+	}
+	return (0);
+}
