308b0f00ff6e44d7bdd068f16fc78e212c334980
Sanae Elamri <assanaae@gmail.com>
2024-06-23 13:45:58 +0100

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a msg followed by a new line,
+ * to the standard error.
+ * Return: returns 1.
+ */
+
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",);
+	return (1);
+}
