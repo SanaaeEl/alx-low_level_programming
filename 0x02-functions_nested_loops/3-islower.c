0e4c28eff166b162f07538f2a20270d691218d13
Sanae Elamri <assanaae@gmail.com>
2024-06-26 02:06:15 +0100

    is lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,15 @@
 
 /**
  * _islower - cheks for lower case characters.
+ * int c: is the palceholder for the input character.
+ * return 1 if the character is a lower case,
+ * and 0 if it's uppercase.
  */
 
 int _islower(int c)
 {
-	if(islower(c))
-		return 1;
+	if (islower(c))
+		return (1);
 	else
-		return 0;
+		return (0);
 }
