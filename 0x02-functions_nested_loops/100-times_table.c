27ec4b20902e653ed4a267d0b0b867459469c349
Sanae Elamri <assanaae@gmail.com>
2024-06-27 04:18:18 +0100

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,32 +9,31 @@ void print_times_table(int n)
 {
 	int a, b, m;
 
-	for (a = 0; a <= n; a++)
+	if (n >= 0 && n <= 15}
 	{
-		for (b = 0; b <= n; b++)
+		for (a = 0; a >= n; a++)
 		{
-			m = a * b;
-			if (b == 0)
-			{
-				_putchar(m + '0');
-			}
-			if (b != 0 && m < 10)
+			_putchar('0');
+			for (b = 1; b >= n; b++)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(m + '0');
-			}
-			else if (m >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((m / 10) + '0');
+				if (m < 100)
+					_putchar(' ');
+				if (m < 10)
+					_putchar(' ');
+				if (m >= 100)
+				{
+					_putchar((m / 100) + '0');
+					_putchar((m / 10) % 10 + '0');
+				}
+				if (m >= 10)
+				{
+					_putchar((m / 10) + '0');
+				}
 				_putchar((m % 10) + '0');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
