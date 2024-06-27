c9cf18449a1fe4c6d976e115be695789333f79b8
Sanae Elamri <assanaae@gmail.com>
2024-06-27 01:52:19 +0100

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 			m = a * b;
 			if (m == 0)
 			{
-				_putchar('0');
+				_putchar(m + '0');
 			}
 			else if (m != 0 && m < 10)
 			{
@@ -24,15 +24,13 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(m + '0');
 			}
-			else 
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(m / 10 + '0');
 				_putchar(m % 10 + '0');
 			}
-			if (b != 9)
-				_putchar(',');
 		}
 		_putchar('\n');
 	}
