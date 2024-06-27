479cfe38ab9afaad875011b779e379fde6233c7a
Sanae Elamri <assanaae@gmail.com>
2024-06-27 02:41:12 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * Return: returns tha addition of two numbers.
+ *@a: first number.
+ *@b: second number.
+ */
+
+int times_table(int a, int b)
+{
+	return (a+b);
+}
