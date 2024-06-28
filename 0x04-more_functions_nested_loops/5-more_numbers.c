8f501d653d527a5ace225cd572ed2c04549a65ae
SanaaeEl <assanaae@gmail.com>
2024-06-28 08:48:50 +0100

    more nb

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,11 +18,13 @@ void more_numbers(void)
 			{
 				_putchar(i + '0');
 			}
-			else
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
+			/**
+			 *else
+			 *{
+			 *	_putchar(i / 10 + '0');
+			 *	_putchar(i % 10 + '0');
+			 *}
+			 */
 		}
 		_putchar('\n');
 		j++;
