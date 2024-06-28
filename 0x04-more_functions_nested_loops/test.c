e2410163779080afb15c2208933daba6884ea9f7
SanaaeEl <assanaae@gmail.com>
2024-06-28 22:03:07 +0100

    print_numbr

---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+
+int main() {
+	/* Write C code here
+	 * _putchar(123 / 10 + '0');
+	 */
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	/* 402 1024 0 -98 */
+	return (0);
+}
