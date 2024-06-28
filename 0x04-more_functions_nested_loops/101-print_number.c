72868efa7305faa9159bccf57fe2f08fc2333318
SanaaeEl <assanaae@gmail.com>
2024-06-28 21:50:01 +0100

    print_numbr

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,11 +10,13 @@ void print_number(int n)
 	unsigned int temp;
 
 	temp = n;
+	/* handle negatvie numbers */
 	if (n < 0)
 	{
 		_putchar('-');
 		temp = -n;
 	}
+	/* print the digits recursively, except tha last one */
 	if (temp / 10 != 0)
 	{
 		print_number(temp / 10);
