61fbb2e7ef36b6ee4df36e698390feafe2d3d4a6
SanaaeEl <assanaae@gmail.com>
2024-06-28 21:32:12 +0100

    print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: integer to print.
+ */
+
+void print_line(int n)
+{
+	int temp = n;
+
+	/* Handle negatvie numbers */
+	if (n < 0)
+	{
+		_putchar('-');
+		temp = -n;
+	}
+	if (temp / 10 != 0)
+	{
+		/* prints the digits recursively except the last one */
+		print_number(temp / 10);
+	}
+	_putchar(temp % 10 + '0');
+}
