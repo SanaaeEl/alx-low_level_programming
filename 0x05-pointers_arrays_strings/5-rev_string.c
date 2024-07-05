25a8c18b9f50a04a932726636cbd45db8aaec778
SanaaeEl <assanaae@gmail.com>
2024-07-06 00:12:24 +0100

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,10 +9,13 @@
 void rev_string(char *s)
 {
 	int i, len;
+	char t;
 
 	len = strlen(s);
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0; i > len / 2; i++)
 	{
-		_putchar(s[i]);
+		t = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = t;
 	}
 }
