14c6412e1b76db83d106e84f0093178bc885f51e
SanaaeEl <assanaae@gmail.com>
2024-07-05 23:53:33 +0100

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * rev_string - reverses a string.
+ * @s: string to be reversed.
+ */
+
+void rev_string(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
