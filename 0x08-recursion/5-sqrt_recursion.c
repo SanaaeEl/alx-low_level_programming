a5df66e1094759ed83e07689c173a96a624fbefe
SanaaeEl <assanaae@gmail.com>
2024-07-11 05:03:52 +0100

    squareroot_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,27 +2,38 @@
 
 
 /**
- * _sqrt_recusion - returns the natural square root of a number.
- * @n: number in question.
+ * find_sqrt - finds the sqrt within a specified range.
+ * @n1: number in question.
+ * @st: start of range.
+ * @end: end of range.
  * Return: the natural square of n.
+ *
  */
 
-int find_sq(int n, int st, int end)
+
+int find_sqrt(int n1, int st, int end)
 {
 	int mid;
 
 	if (st > end)
 		return (-1);
 	mid = (st + end) / 2;
-	if (mid * mid == n)
-		return mid;
-	else if (mid * mid < n)
-		return find_sq(n, mid + 1, end);
+	if (mid * mid == n1)
+		return (mid);
+	else if (mid * mid < n1)
+		return (find_sqrt(n1, mid + 1, end));
 	else
-		return find_sq(n, st, mid - 1);
+		return (find_sqrt(n1, st, mid - 1));
 }
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * _find_sqrt - finds the sqrt within a specified range.
+ * @n: number in question.
+ * Return: the natural square of n.
+ *
+ */
 int _sqrt_recursion(int n)
 {
-	return find_sq(n, 0, n);
+	return (find_sqrt(n, 0, n));
 }
