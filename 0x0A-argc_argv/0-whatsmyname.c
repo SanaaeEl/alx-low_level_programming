e0ed09ed0a385776dcb90bcb6eb62b49df96635d
SanaaeEl <assanaae@gmail.com>
2024-07-13 04:40:04 +0100

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point.
+ * @argc: number of arguments.
+ * @argv[]: array of arguments.
+ * Return: 0 , successful completion.
+ */
+
+
+int main (int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
