3e04ebebcc29b2dd31933b50eef938e89fa89677
SanaaeEl <assanaae@gmail.com>
2024-07-13 04:46:31 +0100

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ * Return: 0 , successful completion.
+ */
+
+
+int main(int argc, char *argv[] __attribute((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
