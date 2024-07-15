5a9bd2c0e0faec9958cc7ad64d47508a3b97e81c
SanaaeEl <assanaae@gmail.com>
2024-07-15 05:06:19 +0100

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,19 +13,19 @@
 
 int main(int argc, char *argv[])
 {
-	int i, cents, coins = 0;
-	int change[5] = {25,10,5,2,1};
-	
+	int i, cents, coins = 0, change[];
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	change[5] = {25, 10, 5, 2, 1};
 	cents = atoi(argv[1]);
 	if (cents <= 0)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
 	else
 	{
