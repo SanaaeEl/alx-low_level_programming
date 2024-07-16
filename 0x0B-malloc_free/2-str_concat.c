91045782e9a4a909645c4004f1c2810a58124bf9
SanaaeEl <assanaae@gmail.com>
2024-07-16 18:27:13 +0100

    str_concat fnlyy

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,32 +12,19 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	unsigned int l1, l2, i;
+	unsigned int l1, l2, i, j;
 
-	if (s1 == NULL && s2 == NULL)
+	l1 = (s1 != NULL) ? strlen(s1) : 0;
+	l2 = (s2 != NULL) ? strlen(s2) : 0;
+	ptr = malloc((l1 + l2 + 1) * sizeof(char));
+	if (ptr == NULL)
+	{
 		return (NULL);
-	else if (s2 == NULL)
-	{
-		return (s1);
 	}
-	else if (s1 == NULL)
-	{
-		return (s2);
-	}
-	else
-	{
-		l1 = strlen(s1);
-		l2 = strlen(s2);
-		ptr = malloc((l1 + l2 + 1) * sizeof(char));
-		if (ptr == NULL)
-			return (NULL);
-		i = 0;
-		while (i < l1)
-		{
-			ptr[i] = s1[i];
-			i++;
-		}
-		strcat(ptr, s2);
-		return (ptr);
-	}
+	for (i = 0; i < l1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < l2; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+	return (ptr);
 }
