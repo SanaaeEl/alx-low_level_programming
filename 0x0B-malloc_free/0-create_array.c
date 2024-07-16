0abe1ab6e9456587af1866a774efc4b4f5cd146d
SanaaeEl <assanaae@gmail.com>
2024-07-16 13:59:45 +0100

    create_arrayyy

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,6 +18,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(sizeof(char) * size);
+	if (ptr == NULL)
+		return (NULL);
 	while (i < size)
 	{
 		ptr[i] = c;
