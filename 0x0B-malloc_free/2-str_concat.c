886f369221dc1d867306e1a78c0ac428b138a946
SanaaeEl <assanaae@gmail.com>
2024-07-16 15:02:19 +0100

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,19 +14,19 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	unsigned int l1, l2, i;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	l1 = strlen(s1);
 	l2 = strlen(s2);
 	ptr = malloc((l1 + l2 + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
 	i = 0;
 	while (i < l1)
 	{
 		ptr[i] = s1[i];
 		i++;
 	}
-	if (ptr == NULL)
-		return (NULL);
 	strcat(ptr, s2);
 	return (ptr);
 }
