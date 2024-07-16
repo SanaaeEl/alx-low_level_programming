e5357954fe71b614603b7ddf8e7b34689f107673
SanaaeEl <assanaae@gmail.com>
2024-07-16 23:44:11 +0100

    free_grid betty

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,7 +3,8 @@
 
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by your alloc_grid function.
+ * free_grid - frees a 2 dimensional grid previously created
+ * by alloc_grid function.
  * @grid: grid previously created.
  * @height: height of the grid.
  */
