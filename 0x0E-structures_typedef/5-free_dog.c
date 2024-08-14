ffc34b712505ebd99be6fe067c9d9dbd78a95c51
SanaaeEl <assanaae@gmail.com>
2024-08-14 06:45:40 +0100

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+
+/**
+ * free_dog - frees dogs
+ * @d: dog to free
+ *
+ */
+
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+	free(d);
+}
