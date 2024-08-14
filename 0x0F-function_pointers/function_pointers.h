82871a2b98163efdd7e6faac3602b58b232063bd
SanaaeEl <assanaae@gmail.com>
2024-08-14 13:30:55 +0100

    print name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONPOINTERS_H
+#define FUNCTIONPOINTERS_H
+
+
+#include <unistd.h>
+
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+
+
+#endif  /** FUNCTIONPOINTERS_H */
