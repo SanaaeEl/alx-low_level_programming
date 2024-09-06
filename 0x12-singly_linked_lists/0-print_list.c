d0d6d2c04433747762fa2144c2af729914e6bc04
SanaaeEl <assanaae@gmail.com>
2024-09-06 07:55:46 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,16 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t elements = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)");
 		else
 			printf("[%u] %s", h->len, h->str);
-		count++;
+		elements++;
 		h = h->next;
 	}
-	return (count);
+	return (elements);
 }
