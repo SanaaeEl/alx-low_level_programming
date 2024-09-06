09629ddba8e9b34a2ce80671a1045415bbb4d90d
SanaaeEl <assanaae@gmail.com>
2024-09-06 16:35:54 +0100

    free listint

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,7 +2,7 @@
 
 
 /**
- * free_listint_t - frees a listint_t list
+ * free_listint - frees a listint_t list
  * @head: pointer to the first element of the list
  */
 void free_listint(listint_t *head)
