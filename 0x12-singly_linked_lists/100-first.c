7d5a85d1c9364ad15ae992255ecb35c87023b420
SanaaeEl <assanaae@gmail.com>
2024-09-06 11:26:17 +0100

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * before_main - prints a string before the main function is executed.
+ * Description: functions marked with the constructor attribute are executed
+ * 		before the main function, which allows to run code during
+ * 		the initialization phase of the program.
+ */
+
+void __attriute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n bore my house
+			upon my back!\n");
+}
