11e2d88140839d6e14f1866d9544738720b9c4b0
SanaaeEl <assanaae@gmail.com>
2024-09-06 16:47:20 +0100

    free listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list, sets the head to NULL
+ * @head: double pointer to the first element of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
