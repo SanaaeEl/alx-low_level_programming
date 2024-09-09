abe1bb628a15c81dd4f750b8ba5541a6fe6bb69c
SanaaeEl <assanaae@gmail.com>
2024-09-09 19:29:52 +0100

    binary to uint

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,21 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+
+
+#include <stdio.h>
+#include <stdib.h>
+#include <string.h>
+
+
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+
+
+
+
+
+
+#endif  /** _MAIN_H */
