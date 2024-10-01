7c0839efcc0ff75c0283c8f69d1db51e2236ce09
SanaaeEl <assanaae@gmail.com>
2024-10-01 15:02:12 +0100

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,7 @@
  * Return: bit value at @index, otherwise -1 if there's an error
  */
 
-int get_index(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -13,7 +13,7 @@
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int u);
-int get_index(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
 
 
 
