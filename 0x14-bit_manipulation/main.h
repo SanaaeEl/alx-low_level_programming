047ca11a3a4ef54248d344abd75f14a22298034d
SanaaeEl <assanaae@gmail.com>
2024-10-01 15:34:15 +0100

    set bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
 
 
 
@@ -14,6 +15,7 @@ int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int u);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 
 
