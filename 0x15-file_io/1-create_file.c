e00f4833f4a2e47523a8116af8e70a49bf6d2d76
SanaaeEl <assanaae@gmail.com>
2024-10-04 12:03:50 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	ssize_t op, wr;
 	int l;
-	
+
 	if (!filename)
 		return (-1);
 	if (!text_content)
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 
 	op = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 	wr = write(op, text_content, l);
-	
+
 	if (op == -1 || wr == -1)
 		return (-1);
 
