d02e1ded64b60c9a3980481e1c2f16bf002f58a8
SanaaeEl <assanaae@gmail.com>
2024-10-06 22:41:50 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,26 +16,47 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	file_from = open(av[1], O_RDONLY);
-	copy = read(file_from, buffer, BUFF);
-	if (file_from == -1 || copy == -1)
+	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s",
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 				av[1]);
 		exit(98);
 	}
 
 	file_to = open(av[2], O_WRONLY | O_TRUNC | O_CREAT,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	paste = write(file_to, buffer, copy);
-	if (file_to == -1 || paste == -1)
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(file_from);
 		exit(99);
+	}
+
+	while((copy = read(file_from, buffer, BUFF)) > 0)
+	{
+		paste = write(file_to, buffer, copy);
+		if (paste == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+					av[2]);
+			close(file_from);
+			close(file_to);
+			exit(99);
+		}
+	}
+
+	if (copy == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				av[0]);
+		close(file_from);
+		close(file_to);
+		exit(98);
 	}
 
 	if (close(file_from) == -1)
