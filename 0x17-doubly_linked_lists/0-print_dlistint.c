8ff8ec595adabbd6a8feb692508bb83b2190b8e8
SanaaeEl <assanaae@gmail.com>
2024-10-09 22:58:45 +0100

    intitial

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: points to the head of the linked list
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const dlistint_t *h)
+{
+	size_t c;
+
+	return (c);
+}
