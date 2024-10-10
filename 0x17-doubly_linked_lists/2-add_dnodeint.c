6b2a5f231da3f343e96838cd814ac9ba130379f2
SanaaeEl <assanaae@gmail.com>
2024-10-10 17:09:26 +0100

    add dnodeint

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,9 +18,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->next = *head;
 	new->prev = NULL;
 
-	*head = new;
 	if (*head != NULL)
 		(*head)->prev = new;
 
+	*head = new;
 	return (new);
 }
